119c5784e02f42f8b05a2061753fed8d5c832b46
juanlax03 <juanlax2003@gmail.com>
2025-04-12 14:29:06 +0200

    controlador rest mitad

---
+++ espacioFisico/src/main/java/dto/CrearEspacioFisicoDto.java
@@ -0,0 +1,77 @@
+package dto;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CrearEspacioFisicoDto {
+	
+	public String nombre;
+    public String propietario;
+    public int capacidad;
+    public String direccion;
+    public double longitud;
+    public double latitud;
+    public String descripcion;
+
+    public CrearEspacioFisicoDto() {}
+    
+    public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPropietario() {
+		return propietario;
+	}
+
+	public void setPropietario(String propietario) {
+		this.propietario = propietario;
+	}
+
+	public int getCapacidad() {
+		return capacidad;
+	}
+
+	public void setCapacidad(int capacidad) {
+		this.capacidad = capacidad;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public double getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(double longitud) {
+		this.longitud = longitud;
+	}
+
+	public double getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(double latitud) {
+		this.latitud = latitud;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
---
+++ espacioFisico/src/main/java/dto/UpdateEspacioFisicoDto.java
@@ -0,0 +1,34 @@
+package dto;
+
+public class UpdateEspacioFisicoDto {
+	
+	private String id, nombre, descripcion;
+	private Integer capacidad;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public Integer getCapacidad() {
+		return capacidad;
+	}
+	public void setCapacidad(Integer capacidad) {
+		this.capacidad = capacidad;
+	}
+	
+	
+}
